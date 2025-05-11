bd5e852d83a652c9742e18520a830d3599a6536f
James Maloney <jamesmaloney43@u.boisestate.edu>
2025-05-11 07:39:58 -0600

    /database returns modified

--- src/main/java/com/heroku/java/GettingStartedApplication.java
+++ src/main/java/com/heroku/java/GettingStartedApplication.java
@@ -10,6 +10,7 @@ import javax.sql.DataSource;
 import java.sql.Connection;
 import java.util.ArrayList;
 import java.util.Map;
+import java.util.Random;
 
 @SpringBootApplication
 @Controller
@@ -30,13 +31,15 @@ public class GettingStartedApplication {
     String database(Map<String, Object> model) {
         try (Connection connection = dataSource.getConnection()) {
             final var statement = connection.createStatement();
-            statement.executeUpdate("CREATE TABLE IF NOT EXISTS ticks (tick timestamp)");
-            statement.executeUpdate("INSERT INTO ticks VALUES (now())");
+            //statement.executeUpdate("CREATE TABLE IF NOT EXISTS ticks (tick timestamp)");
+            //statement.executeUpdate("INSERT INTO ticks VALUES (now())");
+            statement.executeUpdate("CREATE TABLE IF NOT EXISTS table_timestamp_and_rand_string (tick timestamp, random_string varchar(20))");
+            statement.executeUpdate("INSERT INTO table_timestamp_and_rand_string VALUES (now(), '" + getRandomString() + "')");
 
-            final var resultSet = statement.executeQuery("SELECT tick FROM ticks");
+            final var resultSet = statement.executeQuery("SELECT tick, random_string FROM table_timestamp_and_rand_string");
             final var output = new ArrayList<>();
             while (resultSet.next()) {
-                output.add("Read from DB: " + resultSet.getTimestamp("tick"));
+                output.add("Read from DB: " + resultSet.getTimestamp("tick") + " " + resultSet.getString("random_string"));
             }
 
             model.put("records", output);
@@ -50,5 +53,18 @@ public class GettingStartedApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(GettingStartedApplication.class, args);
+    }
+
+    protected String getRandomString() {
+        String SALTCHARS = "ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890";
+        StringBuilder salt = new StringBuilder();
+        Random rnd = new Random();
+        while (salt.length() < 18) { // length of the random string.
+            int index = (int) (rnd.nextFloat() * SALTCHARS.length());
+            salt.append(SALTCHARS.charAt(index));
+        }
+        String saltStr = salt.toString();
+        return saltStr;
+
     }
 }
